98d5aa116caac385fa64b7f16f48553004f9c131
FSReed <1714747596@qq.com>
2024-11-27 13:54:15 +0800

    Configure zsh and oh-my-zsh

---
+++ aliases.zsh
@@ -0,0 +1,11 @@
+alias R='source ~/.zshrc'
+alias mzsh='vim ~/.zshrc'
+alias mvim='vim ~/.vimrc'
+
+# For Git
+alias ga='git add .'
+alias gs='git status'
+alias gc='git commit'
+alias gpo='git push origin'
+alias gl='git log --graph --oneline'
+
