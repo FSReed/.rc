278d571c3319772e28bac8cccbf30b6ee72a4f9e
FSReed <1714747596@qq.com>
2024-12-15 11:41:36 +0800

    Update Configs

--- plugins.zsh
+++ plugins.zsh
@@ -2,8 +2,9 @@
 
 # Get Zsh Plugins
 
-ZSH_CUSTOM=${HOME}/.oh-my-zsh/custom
+ZSH_PLUGIN=${HOME}/.oh-my-zsh/custom/plugins
+
 
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_PLUGIN}/zsh-syntax-highlighting
 
