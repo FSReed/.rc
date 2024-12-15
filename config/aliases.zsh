c10b5dfe3efc9db0698f0a292b7a68befa966013
FSReed <1714747596@qq.com>
2024-12-15 13:00:31 +0800

    Add some conditions

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -11,10 +11,14 @@ alias gc='git commit'
 alias gpo='git push origin'
 alias gl='git log --graph --oneline'
 
-# For xv6 debug
+# For xv6 debugging
 xvdbg () {
         local SESSION_NAME="xv6"
+        # Make sure xv6's path is set to ${HOME}/xv6
         local DIR="${HOME}/${SESSION_NAME}"
+        if ! [[ -f ${DIR} ]]; then
+            echo -e "No xv6 repo found"
+            exit 1
         tmux has-session -t ${SESSION_NAME} 2> /dev/null
         if [ $? != 0 ]
         then
