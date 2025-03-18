8d62f6cc9d6cf006d5f0e558ab69d316514becd3
FSReed <1714747596@qq.com>
2025-03-18 14:03:44 +0800

    1. Use symbolic links to sync configuration files ( see sync_script ) 2. Optimize variable reference.

--- plugins.zsh
+++ plugins.zsh
@@ -8,13 +8,13 @@ plugins=(
     "MichaelAquilina/zsh-you-should-use"
 )
 
-ZSH_CUSTOM_PLUGIN=${HOME}/.oh-my-zsh/custom/plugins
+ZSH_CUSTOM_PLUGIN="${HOME}"/.oh-my-zsh/custom/plugins
 
 for item in ${plugins[@]}; do
     name=$(echo ${item} | cut -d/ -f2)
 
     if ! [[ -d ${ZSH_CUSTOM_PLUGIN}/${name} ]]; then
-        git clone https://github.com/${item}.git ${ZSH_CUSTOM_PLUGIN}/${name}
+        git clone https://github.com/"${item}".git "${ZSH_CUSTOM_PLUGIN}"/"${name}"
     fi
 done
 
